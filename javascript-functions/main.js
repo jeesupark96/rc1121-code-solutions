5395fb9d53a6a3dbbfe88e82e54cce77a4ed04d5
Jeesu Park <jeesupark96@gmail.com>
2021-12-07 13:18:22 -0800

    functions

--- javascript-functions/main.js
+++ javascript-functions/main.js
@@ -1,33 +1,39 @@
 function convertMinutesToSeconds(minutes) {
   var seconds = (minutes * 60);
-  console.log('convertMinutesToSeconds:', seconds);
+  return seconds;
 }
 
-convertMinutesToSeconds(5);
+var convertMinutesToSecondsResult = convertMinutesToSeconds(5);
+console.log('convertMinutesToSecondsResult:', convertMinutesToSecondsResult);
 
 function greet(name) {
-  console.log('"Hey, ' + name + '"');
+  var names = name;
+  return names;
 }
-
-greet('Beavis');
+var greetResult = greet('Beavis');
+console.log('greetResult:', '"Hey, ' + greetResult + '"');
 
 function getArea(width, height) {
   var area = width * height;
-  console.log('getArea results:', area);
+  return area;
 }
 
-getArea(17, 42);
+var getAreaResults = getArea(17, 42);
+console.log('getAreaResult:', getAreaResults);
 
 function getFirstName(person) {
-  var name = person.firstName;
-  console.log('First Name of Person:', name);
+  var first = person.firstName;
+  return first;
 }
+var getFirstNameResults = getFirstName({ firstName: 'Lelouche', lastName: 'Lamperouge' });
 
-getFirstName({ firstName: 'Lelouche', lastName: 'Lamperouge' });
+console.log('getFirstNameResult:', getFirstNameResults);
 
 function getLastElement(array) {
   var last = array[array.length - 1];
-  console.log('result of getLastElement:', last);
+  return last;
 }
 
-getLastElement(['propane', 'and', 'propane', 'accessories']);
+var getLastElementResult = getLastElement(['propane', 'and', 'propane', 'accessories']);
+
+console.log('getLastElementResult:', getLastElementResult);
