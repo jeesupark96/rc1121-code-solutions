7037d55b46d96f6c791070ded26dde41a941b041
Jeesu Park <jeesupark96@gmail.com>
2021-12-10 14:48:17 -0800

    object man1

---
+++ object-manipulation-1/src/set-value.js
@@ -0,0 +1,13 @@
+/* exported setValue */
+// define function named 'setValue' with three parameters, 'object', 'key' and 'value'
+// set the object with a dot notation to the key and assign it to the value
+// return object
+
+function setValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+// define function named 'setValue' with three parameters, 'object', 'key' and 'value'
+// set the object with a bracket notation to the key and assign it to the value
+// return object
