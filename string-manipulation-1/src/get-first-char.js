cb9ad451e4eaf9c88d79e58e0a51d4b5ef672b92
Jeesu Park <jeesupark96@gmail.com>
2021-12-10 11:02:03 -0800

    man 1

--- string-manipulation-1/src/get-first-char.js
+++ string-manipulation-1/src/get-first-char.js
@@ -3,3 +3,7 @@ function getFirstChar(string) {
   var first = string.charAt(0);
   return first;
 }
+
+// define a function named 'getFirstChar' which has a single parameter 'string'
+// call the charAt method to get the first element in the string and assign it to the variable named 'first'
+// return the variable first
--- string-manipulation-1/src/is-upper-cased.js
+++ string-manipulation-1/src/is-upper-cased.js
@@ -4,3 +4,8 @@ function isUpperCased(word) {
     return true;
   } else return false;
 }
+
+// define a function names 'isUpperCased' which is called with a single parameter 'word'
+// call an if statement with one condition, if the result of the slice method on word is strictly equally to the toUpperCase method of word, then the function will be called.
+// if the function is true, return true
+// if the function is not true, return false.
