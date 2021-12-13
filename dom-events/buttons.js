acfedc999c1b25a1227b5c3510d17700a5756be6
Jeesu Park <jeesupark96@gmail.com>
2021-12-13 12:45:50 -0800

    events revised

--- dom-events/buttons.js
+++ dom-events/buttons.js
@@ -1,26 +1,26 @@
 function handleClick(event) {
   console.log('button clicked');
-  console.log(event);
-  console.log(event.target);
+  console.log('event: ', event);
+  console.log('event.target: ', event.target);
 }
 
 var mouseEvent = document.querySelector('.click-button');
 
-mouseEvent.addEventListener('click', handleClick, Boolean);
+mouseEvent.addEventListener('click', handleClick);
 
 function handleMouseover(event) {
   console.log('button hovered');
-  console.log(event);
-  console.log(event.target);
+  console.log('event: ', event);
+  console.log('event.target: ', event.target);
 }
 
-var mouseHover = document.querySelector('.hover-button', Boolean);
+var mouseHover = document.querySelector('.hover-button');
 mouseHover.addEventListener('mouseover', handleMouseover);
 
 function handleDoubleClick(event) {
   console.log('button double clicked');
-  console.log(event);
-  console.log(event.target);
+  console.log('event: ', event);
+  console.log('event.target: ', event.target);
 }
 
 var doubleClick = document.querySelector('.double-click-button');
