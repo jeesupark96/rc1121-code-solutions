e2260ab13c4aeac722b11d8a715082f27562c957
Jeesu Park <jeesupark96@gmail.com>
2021-12-30 15:37:02 -0800

    carousel

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -95,23 +95,23 @@ document.addEventListener('click', function (event) {
 function newFun() {
   if (image.src === 'http://127.0.0.1:5500/javascript-carousel/images/001.png') {
     image.src = 'images/004.png';
-    circle.className = 'far fa-circle';
+    circle.className = 'far fa-circle circleone';
     circleTwo.className = 'fas fa-circle circleblack';
   } else if (image.src === 'http://127.0.0.1:5500/javascript-carousel/images/004.png') {
     image.src = 'images/007.png';
-    circleTwo.className = 'far fa-circle';
+    circleTwo.className = 'far fa-circle circletwo';
     circleThree.className = 'fas fa-circle circleblack';
   } else if (image.src === 'http://127.0.0.1:5500/javascript-carousel/images/007.png') {
     image.src = 'images/025.png';
-    circleThree.className = 'far fa-circle';
+    circleThree.className = 'far fa-circle circlethree';
     circleFour.className = 'fas fa-circle circleblack';
   } else if (image.src === 'http://127.0.0.1:5500/javascript-carousel/images/025.png') {
     image.src = 'images/039.png';
-    circleFour.className = 'far fa-circle';
+    circleFour.className = 'far fa-circle circlefour';
     circleFive.className = 'fas fa-circle circleblack';
   } else if (image.src === 'http://127.0.0.1:5500/javascript-carousel/images/039.png') {
     image.src = 'images/001.png';
-    circleFive.className = 'far fa-circle';
+    circleFive.className = 'far fa-circle circlefive';
     circle.className = 'fas fa-circle circleblack';
   }
 }
