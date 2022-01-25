0f5f3b7b66188f6e3d95e11b56f2aa638d067bf9
Jeesu Park <jeesupark96@gmail.com>
2022-01-25 00:37:59 +0000

    javascript-typing-tutor

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,11 @@
+const letters = document.querySelectorAll('span');
+var i = 0;
+document.addEventListener('keydown', event => {
+
+  if (event.key === letters[i].textContent) {
+    letters[i].className = 'lettergreen';
+    i++;
+    letters[i].className = 'letter borderbottom';
+  } else letters[i].className = 'letterred borderbottomred';
+
+});
