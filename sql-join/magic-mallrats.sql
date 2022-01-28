6abfe9a0433649ada16265ca41ebabacd3f43966
Jeesu Park <jeesupark96@gmail.com>
2022-01-28 00:16:54 +0000

    sql-join

--- sql-join/magic-mallrats.sql
+++ sql-join/magic-mallrats.sql
@@ -1,6 +1,6 @@
-select "cu"."firstName",
-       "cu"."lastName"
-from "customers" as "cu"
+select "c"."firstName",
+       "c"."lastName"
+from "customers" as "c"
 join "rentals" using ("customerId")
 join "inventory" using ("inventoryId")
 join "films" using ("filmId")
