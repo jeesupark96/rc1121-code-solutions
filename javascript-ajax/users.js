0bf3b53e3dc9153c877b94c4d1d0129846e149a9
Jeesu Park <jeesupark96@gmail.com>
2022-02-01 18:27:15 +0000

    javascript-ajax/

--- javascript-ajax/users.js
+++ javascript-ajax/users.js
@@ -1,14 +1,16 @@
 function users(name) {
   var userlist = document.querySelector('#user-list');
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://www.balldontlie.io/api/v1/players');
+  xhr.open('GET', 'https://api-nba-v1.p.rapidapi.com/players/lastName/James');
+  xhr.setRequestHeader('x-rapidapi-host', 'api-nba-v1.p.rapidapi.com');
+  xhr.setRequestHeader('x-rapidapi-key', 'b9229f7371mshb80cdc8bb545c9ap153871jsn9ee1382b317f');
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     console.log(xhr.status);
-    console.log(xhr.response);
-    for (var i = 0; i < xhr.response.length; i++) {
+    console.log(xhr.response.api.players[0]);
+    for (var i = 0; i < xhr.response.api.players.length; i++) {
       var list = document.createElement('li');
-      list.textContent = xhr.response[i].first_name;
+      list.textContent = xhr.response.api.players[i].firstName + ' ' + xhr.response.api.players[i].lastName;
       userlist.appendChild(list);
 
     }
