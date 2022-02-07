bc1dd27eb9ade6f77dfa3758e533e4dc684b7c49
Jeesu Park <jeesupark96@gmail.com>
2022-02-07 23:09:03 +0000

    fetch/

--- fetch/main.js
+++ fetch/main.js
@@ -2,12 +2,12 @@ const fetchResponse = fetch('https://jsonplaceholder.typicode.com/users', {
   method: 'GET'
 })
   .then(response => response.json(fetchResponse))
+  .then(data => console.log(data))
   .catch(console.error);
-console.log(fetchResponse);
 
 const pokemonResponse = fetch('https://pokeapi.co/api/v2/pokemon/4', {
   method: 'GET'
 })
   .then(response => response.json(pokemonResponse))
+  .then(data => console.log(data))
   .catch(console.error);
-console.log([pokemonResponse]);
