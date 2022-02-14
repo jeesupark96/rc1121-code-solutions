5cad2825b0b89b77f8bb0c7929da03d0cd6632b6
Jeesu Park <jeesupark96@gmail.com>
2022-02-14 19:47:16 +0000

    react-file-uploads/

--- react-file-uploads/react-file-uploads/database/data.sql
+++
@@ -1,2 +0,0 @@
-insert into "images" ("caption", "url")
-values ('LFZ Logo', '/images/image-1608316435111.png');
--- react-file-uploads/react-file-uploads/scripts/db-import.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-source .env
-psql $DATABASE_URL -f database/schema.sql -f database/data.sql
