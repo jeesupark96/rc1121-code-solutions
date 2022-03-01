9fc354ef5aa8242c9a41a61be19520b5950801cf
Jeesu Park <jeesupark96@gmail.com>
2022-03-01 01:35:20 +0000

    big-o-notation/

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -24,19 +24,19 @@ function uniqueQuadratic(words) {
     i < words.length;               // 2 * n = O(2n)
     i++                             // 2 * n = O(2n)
   ) {
-    const word = words[i];          // 1 * n = O(n)
+    const word = words[i];          // 2 * n = O(2n)
     let isUnique = true;            // 1 * n = O(n)
     for (
       let c = 0;                    // 1 * n = O(n)
-      c < i;                        // 2 * n = O(n^2)
-      c++                           // 2 * n = O(n^2)
+      c < i;                        // 1 * n^2 = O(n^2)
+      c++                           // 2 * n^2 = O(2(n^2)) ~= O(n^2)
     ) {
-      const comparing = words[c];   // 1 * 1 = O(n^2)
-      if (comparing === word) {     // 1 * n = O(n^2)
-        isUnique = false;           // 1 * 1 = O(n^2)
+      const comparing = words[c];   // 2 * 1 = O(2(n^2)) ~= O(n^2)
+      if (comparing === word) {     // 1 * n^2 = O(n^2)
+        isUnique = false;           // 1 * n^2 = O(n^2)
       }
     }
-    if (isUnique) {                 // 2 * n = O(2n) ~= O(n)
+    if (isUnique) {                 // 1 * n = O(n) ~= O(n)
       unique[unique.length] = word; // 2 * n = O(2n) ~= O(n)
     }
   }
